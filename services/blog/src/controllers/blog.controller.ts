a6d90211cc72d4cb359063e155a9f41c52cf3a59
JayantRautela <jayantrautela0205@gmail.com>
2026-02-02 15:39:03 +0530

    add blog controllers

---
+++ services/blog/src/controllers/blog.controller.ts
@@ -0,0 +1,27 @@
+import { sql } from "../utils/db.js";
+import TryCatch from "../utils/TryCatch.js";
+
+export const getAllBlogs = TryCatch( async (req, res) => {
+  const { searchQuery, category } = req.query;
+  let blogs;
+
+  if (searchQuery && category) {
+    blogs = await sql `SELECT * FROM blogs WHERE (title ILIKE ${"%" + searchQuery + "%"} OR description ILIKE ${"%" + searchQuery + "%"}) AND category = ${category} ORDER BY created_at DESC`;
+  } else if (searchQuery){
+    blogs = await sql `SELECT * FROM blogs WHERE (title ILIKE ${"%" + searchQuery + "%"} OR description ILIKE ${"%" + searchQuery + "%"}) ORDER BY created_at DESC`;
+  } else {
+    blogs = await sql `SELECT * FROM blogs ORDER BY created_at DESC`;
+  }
+
+  res.status(200).json({
+    blogs
+  });
+})
+
+export const getSingleBlog = TryCatch( async (req, res) => {
+  const blog = await sql `SELECT * FROM blogs WHERE id = ${req.params.id}`;
+
+  res.status(200).json({
+    blog
+  });
+})
--- services/blog/src/server.ts
+++ services/blog/src/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import blogRouter from "./routes/blog.route.js";
 
 const app = express();
+app.use(express.json());
+
+app.use('/api/v1', blogRouter);
 
 const PORT = process.env.PORT;
 
